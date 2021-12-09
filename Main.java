bb636c7b506ef502d1aa8c08375b33e62a462e66
GoncaloRodri <gg.rodrigues@campus.fct.unl.pt>
2021-12-09 11:38:09 +0000

    main updated hi
    Signed-off-by: GoncaloRodri <gg.rodrigues@campus.fct.unl.pt>

--- Main.java
+++ Main.java
@@ -2,9 +2,10 @@ public class Main{
     //ola main
 
     public static void main(String[] args) {
-        hi();
+        System.out.println(hi());
     }
 
-    private static void hi(){
+    private static int hi(){
+        return 1;
     }
 }
