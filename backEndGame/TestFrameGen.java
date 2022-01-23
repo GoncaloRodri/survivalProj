e212fd1e1af05b24fc69a2cc8e6f11a4a769aff8
GoncaloRodri <gg.rodrigues@campus.fct.unl.pt>
2022-01-23 17:10:52 +0000

    TestFrame started - check how to
    Signed-off-by: GoncaloRodri <gg.rodrigues@campus.fct.unl.pt>

---
+++ backEndGame/TestFrameGen.java
@@ -0,0 +1,18 @@
+package backEndGame;
+
+import javax.swing.*;
+
+public class TestFrameGen {
+
+    private JFrame frame;
+
+    public TestFrameGen(){
+       setFrame();
+
+    }
+
+    protected void setFrame(){
+        frame = new JFrame("TestFrame");
+        frame.
+    }
+}
